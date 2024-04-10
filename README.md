6d6caddef231cadcfae6475d96d7eb0f7431788e
Ryan Oliveira <jryancoelho79@hotmail.com>
2024-04-09 23:58:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# implementacao-de-expressoes-regulares
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run src/compile.ts
+```
+
+This project was created using `bun init` in bun v1.1.3. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ src/modules/lexer/lexer.ts
@@ -0,0 +1,101 @@
+export enum TokenType {
+  Number,
+  Identifier,
+  Equals,
+  OpenParen,
+  CloseParen,
+  BinaryOperator,
+  Let,
+}
+
+export type Token = {
+  value: string;
+  type: TokenType;
+};
+
+function token(value: string, type: TokenType): Token {
+  return { value, type };
+}
+
+export function tokenize(input: string): Token[] {
+  const tokens: Token[] = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    const char = input[cursor];
+
+    if (/\s/.test(char)
+    ) {
+      cursor++;
+      continue;
+    }
+
+    if (
+      char === "+" ||
+      char === "-" ||
+      char === "*" ||
+      char === "/" ||
+      char === "%" ||
+      char === "^"
+    ) {
+      tokens.push(token("+", TokenType.BinaryOperator ));
+      cursor++;
+      continue;
+    }
+
+    if (char === "=") {
+      tokens.push(token("=", TokenType.Equals));
+      cursor++;
+      continue;
+    }
+
+    if (char === "(") {
+      tokens.push(token("(", TokenType.OpenParen));
+      cursor++;
+      continue;
+    }
+
+    if (char === ")") {
+      tokens.push(token(")", TokenType.CloseParen));
+      cursor++;
+      continue;
+    }
+
+    if (char.match(/[0-9]/)) {
+      let value = "";
+
+      while (input[cursor] && input[cursor].match(/[0-9]/)) {
+        value += input[cursor];
+        cursor++;
+      }
+
+      tokens.push(token(value, TokenType.Number ));
+      continue;
+    }
+
+    if (char.match(/[a-zA-Z]/)) {
+      let value = "";
+
+      while (input[cursor] && input[cursor].match(/[a-zA-Z]/)) {
+        value += input[cursor];
+        cursor++;
+      }
+
+      if (value === "let") {
+        tokens.push(token(value, TokenType.Let ));
+      } else {
+        tokens.push(token(value, TokenType.Identifier));
+      }
+
+      continue;
+    }
+
+    throw new Error(`Invalid character: ${char}`);
+  }
+
+  return tokens;
+}
+
+for (const token of tokenize("let x = 1 + 2 * ( 4 / 3 ) ;")) {
+  console.log(token);
+}
