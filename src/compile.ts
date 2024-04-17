5b8ee51eb0a2a3c85a3e1f395fcd387764d1b207
Ryan Oliveira <jryancoelho79@hotmail.com>
2024-04-17 15:31:32 -0300

    Refactor compile.ts to use Parser class for input processing

--- src/compile.ts
+++ src/compile.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+// import { tokenize } from "./modules/lexer";
+
+// const data = Bun.file(Bun.argv[2]);
+// const source_code = await data.text();
+// for (const token of tokenize(source_code)) {
+//   console.log(token);
+// }
+import Parser from "./modules/parser";
+
+letfy();
+
+function letfy() {
+  const parser = new Parser();
+  console.log("Bem-vindo ao LETFY! Digite 'exit' para sair.");
+  while (true) {
+    const input = prompt("> ");
+
+    if (!input || input === "exit") {
+      break;
+    }
+
+    const program = parser.produceAST(input);
+    console.log(program.body);
+  }
+}
